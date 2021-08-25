2f8a398e291de6495a65da106b668b37b7f91968
Thierry AUZEL <thierry.auzel@gmail.com>
2021-08-25 17:26:06 +0200

    completed front-end

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,34 @@
+class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+
+    if @restaurant.valid?
+      # Rake compatible version
+      redirect_to restaurant_path(@restaurant.id)
+      # Preferred version
+      # redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
